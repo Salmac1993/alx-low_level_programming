37bfa9ea5285b1d64dec6c82c2dcc4087e979a91
Salmac1993 <cdarya356@gmail.com>
2023-08-10 00:08:13 -0700

    &&

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point, prints a specific string to standard error
+ *
+ * Return: Always 1 (error)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
