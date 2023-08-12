5e05c804638b21523e572fb6de9cc08af6da8df0
Salmac1993 <cdarya356@gmail.com>
2023-08-11 17:40:15 -0700

    &&

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Description: Print single-digit numbers separated by commas and spaces,
+ * followed by a newline character.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
 int num;
 
-for (num = 0; num < 10; num++) {
+for (num = 0; num < 10; num++)
+{
 putchar(num + '0');
 
-if (num < 9) {
+if (num < 9)
+{
 putchar(',');
 putchar(' ');
 }
