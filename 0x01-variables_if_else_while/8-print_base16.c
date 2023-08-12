df1006e21c59f96e938c1811223463c5a355fa71
Salmac1993 <cdarya356@gmail.com>
2023-08-11 17:37:10 -0700

    &&

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point
+ *
+ * Description: Print the digits and lowercase hexadecimal characters,
+ * followed by a newline character.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
 char c;
 
-for (c = '0'; c <= '9'; c++) {
+for (c = '0'; c <= '9'; c++)
+{
 putchar(c);
 }
 
-for (c = 'a'; c <= 'f'; c++) {
+for (c = 'a'; c <= 'f'; c++)
+{
 putchar(c);
 }
 
